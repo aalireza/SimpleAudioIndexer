a8382075ccbd87e454de872aaeafccf27cd5d8a5
Alireza Rafiei <alireza.rafiei94@gmail.com>
2016-12-03 22:29:39 -0800

    Restructuring for pip+version bump to 0.4.0

---
+++ SimpleAudioIndexer/__main__.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+import argparse
+import os
+import sys
+
+
+sys.path.append(os.path.dirname(__file__))
+
+
+def argument_handler():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="IBM Watson API Username",
+                        type=str, required=True)
+    parser.add_argument("-p", "--password", help="IBM Watson API Password",
+                        type=str, required=True)
+    parser.add_argument("-d", "--src_dir",
+                        help="Absolute path to ource directory for audio files",
+                        type=str, required=True)
+    parser.add_argument("-s", "--search",
+                        help="Search for a word within the audios of src_dir",
+                        type=str, required=True)
+    parser.add_argument("-t", "--show_timestamps",
+                        help="prints a timestamp of the audio",
+                        action='store_true')
+    parser.add_argument("-m", "--model", type=str,
+                        help=("Model that'd be used for Watson, default is" +
+                              " en-US_BroadbandModel"),
+                        default="en-US_BroadbandModel")
+    parser.add_argument("-v", "--verbose", help="print stage of the program",
+                        action='store_true')
+    args = parser.parse_args()
+    return (args.username, args.password, args.src_dir, args.search,
+            args.show_timestamps, args.model, args.verbose)
+
+
+(username, password, src_dir, word, show_timestamps, model,
+    verbose) = argument_handler()
+indexer = SimpleAudioIndexer(username, password, src_dir, verbose=verbose)
+indexer.index_audio(model=model)
+if show_timestamps:
+    pprint(indexer.get_timestamped_audio())
+pprint(indexer.search_all(word))
---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+from os import path
+import codecs
+import ConfigParser
+
+here = path.abspath(path.dirname(__file__))
+meta_parser = ConfigParser.RawConfigParser()
+meta_parser.read(path.join(here, 'META.txt'))
+
+with codecs.open(path.join(here, 'README.rst'), "rb", "utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name=meta_parser.get("Program", "Name"),
+    version=meta_parser.get("Program", "Version"),
+    description=meta_parser.get("Program", "Description"),
+    long_description=long_description,
+    license=meta_parser.get("Program", "License"),
+    author=meta_parser.get("Author", "Name"),
+    author_email=meta_parser.get("Author", "Email"),
+    packages=["SimpleAudioIndexer"],
+    url=meta_parser.get("Program", "URL"),
+    download_url=(
+        "{}/tarball/{}".format(
+            meta_parser.get("Program", "URL"),
+            meta_parser.get("Program", "TAG"),
+        )
+    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Communications",
+        "Topic :: Multimedia",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=["audio", "indexing", "search", "ibm", "watson"],
+    entry_points={
+          'console_scripts': [
+              'sai = SimpleAudioIndexer:__main__.py',
+          ],
+      },
+)
